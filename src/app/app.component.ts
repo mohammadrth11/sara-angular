9678e6c5a89db4585a10ad17559393afdeab7af9
ahmed mahmoud <ahmedmahmuuud@gmail.com>
2025-05-27 01:02:28 +0300

    Add: some libs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, effect, inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Slider } from 'primeng/slider';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Listbox } from 'primeng/listbox';
+import { ChartModule } from 'primeng/chart';
+import { HttpClient } from '@angular/common/http';
+import { filter, map } from 'rxjs';
 
+interface City {
+  name: string;
+  code: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet], 
+  imports: [FormsModule, Slider, CommonModule, Listbox, ChartModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'sara-project';
+
+  
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,21 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import nora from '@primeng/themes/nora';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideHttpClient(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(),
+     providePrimeNG({
+            theme: {
+                preset: nora
+            }
+    })
+  ],
 };
