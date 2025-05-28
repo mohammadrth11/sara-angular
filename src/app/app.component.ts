ecc1710809daf4b9875ced4355899e7bbbd0e970
mohammadrth11 <mohammad.rth11@gmail.com>
2025-05-29 00:35:17 +0400

    create primrng components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { AfterViewInit, Component, effect, inject, PLATFORM_ID } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  effect,
+  inject,
+  PLATFORM_ID,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Slider } from 'primeng/slider';
@@ -7,19 +13,124 @@ import { Listbox } from 'primeng/listbox';
 import { ChartModule } from 'primeng/chart';
 import { HttpClient } from '@angular/common/http';
 import { filter, map } from 'rxjs';
+import { ColorPicker } from 'primeng/colorpicker';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { TreeNode } from 'primeng/api';
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { AutoComplete } from 'primeng/autocomplete';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { DatePickerModule } from 'primeng/datepicker';
+import { DatePicker } from 'primeng/datepicker';
+import { EditorModule } from 'primeng/editor';
+import { Editor } from 'primeng/editor';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { SelectModule } from 'primeng/select';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 interface City {
   name: string;
   code: string;
 }
+interface AutoCompleteCompleteEvent {
+  originalEvent: Event;
+  query: string;
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, Slider, CommonModule, Listbox, ChartModule],
+  imports: [
+    FormsModule,
+    Slider,
+    CommonModule,
+    Listbox,
+    ChartModule,
+    FormsModule,
+    ColorPicker,
+    Slider,
+    ButtonModule,
+    RippleModule,
+    OrganizationChartModule,
+    AutoComplete,
+    AutoCompleteModule,
+    DatePicker,
+    Editor,
+    InputTextModule,
+    FloatLabelModule,
+    InputNumberModule,
+    SelectModule,
+    InputGroupAddonModule,
+    InputGroupModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  color: string | undefined;
+  value!: number;
+  data: TreeNode[] = [
+    {
+      label: 'F.C Barcelona',
+      expanded: true,
+      children: [
+        {
+          label: 'Argentina',
+          expanded: true,
+          children: [
+            {
+              label: 'Argentina',
+            },
+            {
+              label: 'France',
+            },
+          ],
+        },
+        {
+          label: 'France',
+          expanded: true,
+          children: [
+            {
+              label: 'France',
+            },
+            {
+              label: 'Morocco',
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  items: any[] = [];
 
-  
+  // value: any;
+
+  search(event: AutoCompleteCompleteEvent) {
+    this.items = [...Array(10).keys()].map((item) => event.query + '-' + item);
+  }
+
+  date: Date | undefined;
+  text: string | undefined;
+
+  value2: string | undefined;
+
+  text1: string | undefined;
+
+  text2: string | undefined;
+
+  number: string | undefined;
+
+  selectedCity: City | undefined;
+
+  cities: City[] = [
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' },
+  ];
 }
